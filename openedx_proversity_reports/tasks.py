4042321302cbd9f0bed118f497265e0505d1a331
Squirrel18 <daniel.quiroga@edunext.co>
2019-05-06 18:57:43 -0500

    Adding additional report and required fields.

--- openedx_proversity_reports/tasks.py
+++ openedx_proversity_reports/tasks.py
@@ -7,6 +7,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
+
+from openedx_proversity_reports.edxapp_wrapper.get_course_blocks import get_course_blocks
+from openedx_proversity_reports.edxapp_wrapper.get_modulestore import get_modulestore
+from openedx_proversity_reports.edxapp_wrapper.get_student_library import user_has_role, get_course_staff_role
+from openedx_proversity_reports.reports.last_page_accessed import get_last_page_accessed_data, get_exit_count_data
 from openedx_proversity_reports.utils import generate_report_as_list, get_root_block
 
 
@@ -47,41 +52,20 @@ def generate_completion_report(courses, block_report_filter):
 @task(default_retry_delay=5, max_retries=5)  # pylint: disable=not-callable
 def generate_last_page_accessed_report(courses):
     """
-    Return the last page accessed data for the given courses
+    Return the last page accessed data for the given courses.
+
+    The dictionary contains 'last_page_data' that contains information about the last problem
+    or html or video, etc, that user has accessed.
+    exit_count_data holds information about all units in the course and the count of how many users are in each unit.
     """
-    data = {}
+    report_data = {
+        'last_page_data': {},
+        'exit_count_data': {},
+    }
 
-    for course_id in courses:
-        try:
-            course_key = CourseKey.from_string(course_id)
-        except InvalidKeyError:
-            continue
+    last_page_data = get_last_page_accessed_data(courses)
+    if last_page_data:
+        report_data['last_page_data'] = last_page_data
+        report_data['exit_count_data'] = get_exit_count_data(last_page_data, courses)
 
-        # Getting all students enrolled on the course except staff users
-        enrolled_students = User.objects.filter(
-            courseenrollment__course_id=course_key,
-            courseenrollment__is_active=1,
-            is_staff=0,
-        )
-
-        user_data = []
-        for user in enrolled_students:
-            last_completed_child_position = BlockCompletion.get_latest_block_completed(user, course_key)
-
-            if last_completed_child_position:
-                page_url = reverse(
-                    'jump_to',
-                    kwargs={
-                        'course_id': last_completed_child_position.course_key,
-                        'location': last_completed_child_position.block_key
-                    }
-                )
-                user_data.append({
-                    'username': user.username,
-                    'last_time_accessed': str(last_completed_child_position.modified),
-                    'page_url': page_url,
-                })
-
-        data[course_id] = user_data
-
-    return data
+    return report_data
