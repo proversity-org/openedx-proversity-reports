cb5530a6cbb6c5fd950d59b29094bee946df209b
Squirrel18 <daniel.quiroga@edunext.co>
2019-05-17 16:32:24 -0500

    Getting user role, team, and cohort. Add incremental vertical position.

--- openedx_proversity_reports/edxapp_wrapper/get_student_library.py
+++ openedx_proversity_reports/edxapp_wrapper/get_student_library.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 def user_has_role(*args, **kwargs):
-    """ Get user_has_role method """
+    """ Get user_has_role method. """
 
     backend_function = settings.OPR_STUDENT_LIBRARY
     backend = import_module(backend_function)
@@ -13,9 +13,18 @@ def user_has_role(*args, **kwargs):
 
 
 def get_course_staff_role(*args, **kwargs):
-    """ Get staff role """
+    """ Get staff role. """
 
     backend_function = settings.OPR_STUDENT_LIBRARY
     backend = import_module(backend_function)
 
     return backend.course_staff_role_backend(*args, **kwargs)
+
+
+def course_access_role():
+    """ Get CourseAccessRole model. """
+
+    backend_function = settings.OPR_STUDENT_LIBRARY
+    backend = import_module(backend_function)
+
+    return backend.course_access_role()
